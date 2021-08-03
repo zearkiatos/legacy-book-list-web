f8c58a27de025a2b051853ca107bb9928002304f
Pedro Capriles M <p0c04cw@m-c02z9029lvcg.local.homeoffice.wal-mart.com>
2021-08-03 09:49:11 -0400

    Containers - Connecting Redux to React and add github actions

---
+++ src/containers/BookList/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return <li key={book.title} className="list-group-item">{book.title}</li>;
+    });
+  }
+  render() {
+    <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+  }
+}
