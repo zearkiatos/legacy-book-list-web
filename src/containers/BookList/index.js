cd3a73fcbc19003079f8ebac1fc6a481e49a92f0
zearkiatos <caprilespe@outlook.com>
2021-08-05 21:16:32 -0400

    Implementation of a container class and configure kubernetes service and deployment

--- src/containers/BookList/index.js
+++ src/containers/BookList/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
-      return <li key={book.title} className="list-group-item">{book.title}</li>;
+      return (
+        <li key={book.title} className="list-group-item">
+          {book.title}
+        </li>
+      );
     });
   }
   render() {
-    <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books,
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
